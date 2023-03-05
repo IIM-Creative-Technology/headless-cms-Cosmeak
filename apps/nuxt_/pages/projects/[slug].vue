0e4231e110b0880bf3bc050ae6e08f5eef0401a2
Cosmeak <guillaumefine09@gmail.com>
2023-03-05 17:30:29 +0100

    feat(nuxt): one project page

---
+++ apps/nuxt_/pages/projects/[slug].vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+const { findOne } = useStrapi();
+const slug = useRoute().params.slug;
+const project = ref();
+const previousProject = ref();
+const nextProject = ref();
+
+onMounted(async () => {
+	project.value = await findOne(`projects?filters[slug]=${slug}&populate=deep`);
+	project.value = project.value.data[0];
+	
+	defineHead();
+
+	previousProject.value = await findOne(`projects`, project.value.id - 1);
+	nextProject.value = await findOne(`projects`, project.value.id + 1);
+	console.table(previousProject.value.data.id)
+	console.table(nextProject.value.data.id)
+});
+
+function defineHead() {
+	useHead({
+		title: 'Cosmeak - ' + project.value.name,
+	});
+}
+</script>
+
+<template>
+	<div class="container mx-auto">
+		<div class="flex justify-between mb-8 relative">
+			<NuxtLink class="bg-white px-2 py-1 rounded shadow text-secondary hover:bg-secondary hover:text-light duration-200" v-if="previousProject?.data.id" :to="'/projects/' + previousProject.data.slug">Previous</NuxtLink>
+			<NuxtLink to="/#projects" class="bg-white px-2 py-1 rounded shadow hover:text-light hover:bg-dark duration-200">Retour à l'accueil</NuxtLink>
+			<NuxtLink class="bg-white px-2 py-1 rounded shadow text-secondary hover:bg-secondary hover:text-light duration-200" v-if="nextProject?.data.id" :to="'/projects/' + nextProject.data.slug">Next</NuxtLink>	
+		</div>
+		<Card>
+			<template #body>
+				<div class="w-full h-[250px] relative rounded overflow-hidden mt-2" :class="[project?.thumbnail?.url ? 'text-secondary' : 'bg-secondary-lighten text-dark']">
+					<h1 class="text-5xl text-center drop-shadow-lg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">{{ project?.name }}</h1>
+					<img :src="project?.thumbnail?.url" alt="" class="object-cover h-full w-full" v-if="project?.thumbnail?.url">
+				</div>
+	
+				<div class="mt-8" v-html="project?.content"></div>
+			</template>
+		</Card>
+	
+		<div class="flex gap-2 mt-4">
+			<p class="my-auto">Tags:</p>
+			<span class="p-1 border-secondary border text-secondary text-sm rounded" v-for="(techno, index) in project?.technologies" :key="index">
+				{{ techno?.name }}
+			</span>
+		</div>
+	</div>
+</template>
