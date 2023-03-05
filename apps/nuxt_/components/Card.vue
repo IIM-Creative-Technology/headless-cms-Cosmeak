d8f7975a607fc9e87eb34289c103a2e75dd8b708
Cosmeak <guillaumefine09@gmail.com>
2023-03-05 17:04:59 +0100

    feat(nuxt): card component

---
+++ apps/nuxt_/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="bg-white shadow text-dark rounded overflow-auto flex flex-col">
+		<div class="bg-secondary text-light p-2 text-xl" v-if="$slots.header">
+			<slot name="header" />
+		</div>
+
+		<div class="px-4 py-2">
+			<slot name="body" />
+		</div>
+
+		<div v-if="$slots.footer" class="p-2 bg-gray-50 text-gray-500 text-sm w-full mt-auto" >
+			<slot name="footer" />
+		</div>
+	</div>
+</template>
