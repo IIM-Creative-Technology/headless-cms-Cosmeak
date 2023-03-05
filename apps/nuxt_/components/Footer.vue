2b7b7c7c4105ba519c916815307f160cd0b4b149
Cosmeak <guillaumefine09@gmail.com>
2023-03-05 17:04:21 +0100

    feat(nuxt): page layout

---
+++ apps/nuxt_/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+	<footer class="bg-gray-800 text-white py-10">
+		<div class="container mx-auto px-4">
+			<p class="text-center">&copy; 2023 Cosmeak. Tous droits réservés.</p>
+		</div>
+	</footer>
+</template>
---
+++ apps/nuxt_/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+	<header class="sticky w-full top-0 bg-white shadow-md">
+      <div class="container mx-auto px-4">
+        <nav class="flex justify-between items-center py-4">
+          <div class="text-xl font-semibold">
+            <router-link to="/"><span class="mr-1">🚀</span> Cosmeak</router-link>
+          </div>
+          <div class="flex space-x-4">
+            <router-link to="/" class="text-gray-600 hover:text-gray-900">Accueil</router-link>
+            <router-link to="/projects" class="text-gray-600 hover:text-gray-900">Projets</router-link>
+            <router-link to="/contact" class="text-gray-600 hover:text-gray-900">Contact</router-link>
+          </div>
+        </nav>
+      </div>
+    </header>
+</template>
